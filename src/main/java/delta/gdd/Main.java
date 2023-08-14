460927bd0c066cd67fc86e276908a27cc9a0983e
Damien Morcellet <dmorcellet@free.fr>
2023-08-14 23:02:20 +0200

    Project setup

---
+++ src/main/java/delta/gdd/Main.java
@@ -0,0 +1,16 @@
+package delta.gdd;
+
+/**
+ * Entry point for the Generic Data Decoder.
+ * @author DAM
+ */
+public class Main
+{
+  /**
+   * Main method for this app.
+   * @param args Not used (yet).
+   */
+  public static void main(String[] args)
+  {
+  }
+}
